21cb3b968f7647d800282173c1fe827a9b4b8634
LIAN <qlianab@gpu9.math.ust.hk>
2020-05-29 15:57:23 +0800

    auto

--- config/resnet_cifar/constraint_56.sh
+++ config/resnet_cifar/constraint_56.sh
@@ -4,7 +4,7 @@ python train_constraint_bn_v2_1.py --model resnet_constraint56 \
     --batch-size 128 \
     --constraint_lr 0.005 \
     --constraint_decay 1 \
-    --lambda_constraint_weight 0 \
+    --lambda_constraint_weight 0.001 \
     --lambda_weight_mean 10 \
     --decrease_affine_lr 0.1 \
 
