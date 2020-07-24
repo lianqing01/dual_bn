8a343d8bf8654ecc9b577a06ff86c88ab8c198e7
LIAN <qlianab@gpu9.math.ust.hk>
2020-07-24 09:37:51 +0800

    auto

--- config/cifar100/resnet18_constraint_noise.sh
+++ config/cifar100/resnet18_constraint_noise.sh
@@ -1,15 +1,17 @@
 python train_constraint_bn_v2_1.py \
-    --model resnet_constraint18 \
-    --log_dir vgg/resnet18_constraint_bsz_128_noise_+$1 \
+    --model $1 \
+    --log_dir noise_mean_$7_var_$8_$1+$2+weight+$3+mean+$4+decay+$5+affine_decay+$6 \
+    --norm_layer $2 \
     --lr 0.1 \
     --constraint_lr 0.01 \
     --batch-size 128 \
     --dataset CIFAR100 \
-    --constraint_decay 0.1 \
-    --lambda_constraint_weight 10 \
-    --lambda_weight_mean 1 \
+    --constraint_decay $5 \
+    --lambda_constraint_weight $3 \
+    --lambda_weight_mean $4 \
     --grad_clip 1 \
     --decrease_affine_lr 1 \
+    --affine_decay $6 \
     --sample_noise True \
-    --noise_mean_std $1 \
-    --noise_var_std $1 \
+    --noise_mean_std $7 \
+    --noise_var_std $8
